3f265ba288cfb15717a588d0db1a118ab715a24e
chakshukansal <ckansal8174@gmail.com>
2024-12-03 14:52:46 +0530

    Added

---
+++ Frontend/src/components/Caraousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Caraousel = () => {
+  return (
+    <div className='h-[30vh] bg-zinc-800'>
+        <img className='h-[30vh]' alt="hero"></img>
+    </div>
+  )
+}
+
+export default Caraousel
